4e3aa76a29525f5d70e0c3a8670d4d72790f1ee9
Fajar Chaeril Azhar <chaeril.fajar@gmail.com>
2019-10-06 17:31:36 +0700

    Remove unused enum

--- app/src/main/java/io/fajarca/movies/vo/Result.kt
+++ app/src/main/java/io/fajarca/movies/vo/Result.kt
@@ -6,8 +6,7 @@ data class Result<out T>(val status: Status, val data : T?, val message : String
     enum class Status {
         SUCCESS,
         ERROR,
-        LOADING,
-        EMPTY
+        LOADING
     }
 
     companion object {
@@ -21,10 +20,6 @@ data class Result<out T>(val status: Status, val data : T?, val message : String
 
         fun <T> loading(data: T? = null): Result<T> {
             return Result(Status.LOADING, data, null)
-        }
-
-        fun <T> empty() : Result<T> {
-            return Result(Status.EMPTY, null, null)
         }
     }
 
